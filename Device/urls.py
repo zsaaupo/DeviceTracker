3572e789bc281c61cf404a96781f01fa0590f8b0
zsaaupo <zakariasarker407@gmail.com>
2024-02-05 08:10:15 +0600

    assign device api created

--- Device/urls.py
+++ Device/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import ApiAddDevice
+from .views import ApiAddDevice, ApiAssignDevice
 
 urlpatterns = [
     path('add_device/', ApiAddDevice.as_view()),
+    path('assign_device/', ApiAssignDevice.as_view()),
 ]
--- Device/views.py
+++ Device/views.py
@@ -3,13 +3,17 @@ from django.contrib.auth.models import Group, Permission
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import BasePermission
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_409_CONFLICT
-from .models import Device
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_200_OK, HTTP_409_CONFLICT
+from .models import Device, AssignDevice
 from Company.models import Company, Employee
 
 class IsCompanyUser(BasePermission):
     def has_permission(self, request, view):
         return request.user.groups.filter(name='Company').exists()
+
+class IsEmployeeUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='Employee').exists()
 
 class ApiAddDevice(CreateAPIView):
 
@@ -25,10 +29,6 @@ class ApiAddDevice(CreateAPIView):
             if 'device_type' not in data or data['device_type'] == '':
                 result['massage'] = "device_type can not be null."
                 result['error'] = "device_type"
-                return Response(result, status=HTTP_400_BAD_REQUEST)
-            if 'company_email' not in data or data['company_email'] == '':
-                result['massage'] = "company_email can not be null."
-                result['error'] = "company_email"
                 return Response(result, status=HTTP_400_BAD_REQUEST)
             if 'device_model' not in data or data['device_model'] == '':
                 result['massage'] = "device_model can not be null."
@@ -64,6 +64,66 @@ class ApiAddDevice(CreateAPIView):
             result['message'] = "Success"
             result['Device name'] = data['name']
             return Response(result)
+
+        except Exception as ex:
+            return Response(str(ex))
+
+
+class ApiAssignDevice(CreateAPIView):
+
+    permission_classes = [IsCompanyUser or IsEmployeeUser]
+    def post(self, request):
+        result = {}
+        try:
+            data = request.data
+            if 'start_date' not in data or data['start_date'] == '':
+                result['massage'] = "start_date can not be null."
+                result['error'] = "start_date"
+                return Response(result, status=HTTP_400_BAD_REQUEST)
+            if 'end_date' not in data or data['device_type'] == '':
+                result['massage'] = "device_type can not be null."
+                result['error'] = "device_type"
+                return Response(result, status=HTTP_400_BAD_REQUEST)
+            if 'employee_email' not in data or data['employee_email'] == '':
+                result['massage'] = "employee_email can not be null."
+                result['error'] = "employee_email"
+                return Response(result, status=HTTP_400_BAD_REQUEST)
+
+            if 'device_name' not in data or data['device_name'] == '':
+                result['massage'] = "device_name can not be null."
+                result['error'] = "device_name"
+                return Response(result, status=HTTP_400_BAD_REQUEST)
+
+            device = Device.objects.filter(name=data['device_name'], status="Available").first()
+
+            employee = Employee.objects.filter(employee_email=data['employee_email']).first()
+
+            if device:
+                assign = AssignDevice()
+                assign.device = device
+                assign.employee = employee
+                assign.start_date = data['start_date']
+                assign.end_date = data['end_date']
+                assign.condition_before = data['condition_before']
+                assign.condition_after = data['condition_after']
+                assign.notes = data['notes']
+
+                if employee:
+                    assign.employee = employee
+                else:
+                    result['status'] = HTTP_400_BAD_REQUEST
+                    result['message'] = "Employee not found"
+                    return Response(result)
+                assign.save()
+
+                result['status'] = HTTP_200_OK
+                result['message'] = "Assigned"
+                return Response(result)
+
+            else:
+                result['status'] = HTTP_409_CONFLICT
+                result['message'] = "Device not avaiable"
+                return Response(result)
 
         except Exception as ex:
             return Response(str(ex))
